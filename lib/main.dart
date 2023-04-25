a24f16e716a6d559d8e58f1bfd747be3aa5b91ee
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-24 21:54:40 -0700

    UPD: Basic configuration to set route, theme and MultiProvider in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'app/configs/router/app_router.dart';
+import 'app/configs/theme/app_theme.dart';
+import 'app/data/repositories_impl/cats_repository_impl.dart';
+import 'app/data/services/the_cat_api.dart';
+import 'app/domain/repositories/cat_repository.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<CatRepository>(
+          create: (_) => CatsRepositoryImpl(
+            TheCatApi(),
+          ),
+        ),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +27,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: 'Cat Breeds App',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).getTheme(),
     );
   }
 }
