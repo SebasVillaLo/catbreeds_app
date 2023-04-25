52491682850beb69416e066c43ef5b9a3b4859d6
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-24 21:39:01 -0700

    ADD: New file with the app routes

---
+++ lib/app/configs/router/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/details_screen',
+      name: DetailsScreen.name,
+      builder: (context, state) => const DetailsScreen(),
+    ),
+  ],
+);
