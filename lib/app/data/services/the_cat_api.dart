5d932d211b94d12b4b0c3e0dee01cca93b52075e
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-24 21:44:35 -0700

    ADD: New class to obtain the data from the api

---
+++ lib/app/data/services/the_cat_api.dart
@@ -0,0 +1,34 @@
+// import 'dart:developer' as developer;
+
+import 'package:dio/dio.dart';
+
+import '../../domain/models/cat_breeds_model.dart';
+
+class TheCatApi {
+  /// Instancia of [Dio]
+  final _dio = Dio();
+
+  Future<List<CatModel>> getCats() async {
+    /// [header] is the header of the request with the api key
+    final header = {'x-api-key': 'bda53789-d59e-46cd-9bc4-2936630fde39'};
+    try {
+      final response = await _dio.get(
+        'https://api.thecatapi.com/v1/breeds',
+        options: Options(headers: header),
+      );
+      if (response.statusCode == 200) {
+        final List<dynamic> data = response.data;
+        final List<Map<String, dynamic>> dataMap =
+            List<Map<String, dynamic>>.from(data);
+        final List<CatModel> cats = dataMap
+            .map((Map<String, dynamic> e) => CatModel.fromJson(e))
+            .toList();
+        return cats;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return [];
+  }
+}
+// TODO: Obtener imagen en https://cdn2.thecatapi.com/images/${reference_image_id}
