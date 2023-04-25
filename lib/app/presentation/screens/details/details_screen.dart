77b077f4c21dbf105d6dd9d9c07f6cdf66b7ee06
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-24 21:50:23 -0700

    ADD: Body of the DetailsScreen

---
+++ lib/app/presentation/screens/details/details_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+  static const String name = 'details_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
