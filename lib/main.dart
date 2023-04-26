17d67f12b56339e9310f38bd5cce56956a0af608
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-26 15:22:43 -0700

    UPD: Added new provider and logic to implement the themes

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'app/configs/router/app_router.dart';
-import 'app/configs/theme/app_theme.dart';
 import 'app/data/repositories_impl/cats_repository_impl.dart';
 import 'app/data/services/the_cat_api.dart';
 import 'app/domain/repositories/cat_repository.dart';
+import 'app/presentation/providers/theme_provider.dart';
 
 void main() {
   runApp(
@@ -16,22 +16,31 @@ void main() {
             TheCatApi(),
           ),
         ),
+        Provider<ThemeProvider>(
+          create: (_) => ThemeProvider(),
+        ),
       ],
-      child: const MainApp(),
+      child: const MyApp(),
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Cat Breeds App',
-      routerConfig: appRouter,
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 2).getTheme(),
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      builder: (context, _) {
+        final themeProvider = context.watch<ThemeProvider>();
+        return MaterialApp.router(
+          title: 'Cat Breeds App',
+          routerConfig: appRouter,
+          debugShowCheckedModeBanner: false,
+          theme: themeProvider.theme,
+        );
+      },
     );
   }
 }
