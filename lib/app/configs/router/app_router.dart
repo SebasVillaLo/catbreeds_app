af62ee1bc8dfcb56834783be15db0cd3b9818963
Sebastian Villa Lopez <villalopezsebastian@gmail.com>
2023-04-26 15:37:00 -0700

    UPD: Configuration of the new route and new home

--- lib/app/configs/router/app_router.dart
+++ lib/app/configs/router/app_router.dart
@@ -1,9 +1,10 @@
 import 'package:go_router/go_router.dart';
 
+import '../../domain/models/cat_breeds_model.dart';
 import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/splash_screen',
   routes: [
     GoRoute(
       path: '/',
@@ -13,7 +14,15 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/details_screen',
       name: DetailsScreen.name,
-      builder: (context, state) => const DetailsScreen(),
+      builder: (context, state) {
+        CatModel cat = state.extra as CatModel;
+        return DetailsScreen(cat: cat);
+      },
+    ),
+    GoRoute(
+      path: '/splash_screen',
+      name: SplashScreen.name,
+      builder: (context, state) => const SplashScreen(),
     ),
   ],
 );
